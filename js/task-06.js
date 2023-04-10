6cf5f273b97d4ed74f01ba800385451fcedb3973
Grigoriy Chernish <cga.jsdev@gmail.com>
2023-04-10 17:59:09 +0300

    change task-06

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   const inputValueLength = event.target.value.length;
-  const inputLength = +event.target.dataset.length;
+  const inputLength = Number(event.target.dataset.length);
 
   if (inputValueLength === inputLength) {
     input.classList.add('valid');
